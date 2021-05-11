8a33a40b5cea8eb5c3542cb76f5203f1ff686833
robinweisenburger <robin@weisenburger.me>
2021-05-11 22:19:16 +0200

    getCharge moved and conditional replaced with polymorphism

--- src/main/NewReleasePrice.java
+++ src/main/NewReleasePrice.java
@@ -6,4 +6,7 @@ public class NewReleasePrice extends Price{
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+    public double getCharge(int daysRented){
+        return daysRented * 3;
+    }
 }
--- src/main/Price.java
+++ src/main/Price.java
@@ -1,5 +1,7 @@
 package main;
 
 public abstract class Price {
+
     abstract int getPriceCode();
+    public abstract double getCharge(int daysRented);
 }
